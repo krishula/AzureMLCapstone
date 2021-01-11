1efc211cfb23688d12add37b40aaa5c91e38de87
krishula <krishula.sinha@gmail.com>
2021-01-11 15:24:11 +0000

    Add files via upload

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import argparse
 import os
 import numpy as np
 from sklearn.metrics import mean_squared_error
+from sklearn.preprocessing import StandardScaler
 import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
@@ -10,32 +11,27 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
+run = Run.get_context()
 
 def clean_data(data):
-    # Dict for cleaning data
-    months = {"jan":1, "feb":2, "mar":3, "apr":4, "may":5, "jun":6, "jul":7, "aug":8, "sep":9, "oct":10, "nov":11, "dec":12}
-    weekdays = {"mon":1, "tue":2, "wed":3, "thu":4, "fri":5, "sat":6, "sun":7}
-
-    # Clean and one hot encode data
     x_df = data.to_pandas_dataframe().dropna()
-    y_df = x_df(['target'])
-    x_df.drop(['target'])
-    
-    return x_df,y_df
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+    categorical_val = []
+    for column in x_df.columns:
+        if len(x_df[column].unique()) <= 10:
+            categorical_val.append(column)
 
-ds = TabularDatasetFactory.from_delimited_files('heart.csv')
+    categorical_val.remove('target')
+    x_df = pd.get_dummies(x_df, columns = categorical_val)
 
-x, y = clean_data(ds)
+    s_sc = StandardScaler()
+    col_to_scale = ['age', 'trestbps', 'chol', 'thalach', 'oldpeak']
+    x_df[col_to_scale] = s_sc.fit_transform(x_df[col_to_scale])
 
-x_train,x_test,y_train,y_test = train_test_split(x,y,random_state=0)
 
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
+    y_df = x_df.target
+    x_df = x_df.drop('target', axis=1)
+    return x_df, y_df
 
 def main():
     # Add arguments to script
@@ -48,14 +44,19 @@ def main():
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
+    
+    factory = TabularDatasetFactory()
+    url = 'https://raw.githubusercontent.com/krishula/AzureMLCapstone/main/heart.csv'
+    trainds = factory.from_delimited_files(url)
+
+    x, y = clean_data(trainds)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.1, random_state = 1) 
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
-
-    os.makedirs("outputs",exist_ok=True)
-    joblib.dump(value=model,filename="./outputs/model.joblib")
+    joblib.dump(model, 'outputs/heart-diesease-hyper-model.joblib')
 
 if __name__ == '__main__':
     main()
